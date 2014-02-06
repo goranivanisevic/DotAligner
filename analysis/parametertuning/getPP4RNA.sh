09a0a21a07048d727e184ca3ffd7e60ade7cdcf1
stesee <stesee@gamma01.local>
2014-02-06 15:00:17 +1100

    update of scripts for parametertuning

--- analysis/parametertuning/getPP4RNA.sh
+++ analysis/parametertuning/getPP4RNA.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+#
+# generate seperate fasta file and pp file (basepair probabilities calculated by 'RNAfold -p -d2 --noLP')
+# from an input fasta file listing many sequences (sequences span only one line per identifier)
+#
+
 PDOTALIGNER='/media/data/phd/projects/DotAligner'
-RNAINPUT=${PDOTALIGNER}/analysis/rfam/selected_PIDs/seed_56_95.fasta
+#RNAINPUT=${PDOTALIGNER}/analysis/rfam/selected_PIDs/seed_56_95.fasta
+#RNAINPUT=${PDOTALIGNER}/analysis/benchmarking_seqs/sample_pairwise_aligns.fasta
+RNAINPUT=$1
 
 for ID in `cat ${RNAINPUT} | awk '/^>/'`;
 do
