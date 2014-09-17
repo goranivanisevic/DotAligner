067d60ad2dcbb71f86baa13a36389e6a6451c0e9
noncodo <martinalexandersmith@gmail.com>
2014-09-17 16:15:39 +1000

    Re-inserted Stefan's beautiful PVclust hacks.

--- bin/bigRedButton/clusterStrap.R
+++ bin/bigRedButton/clusterStrap.R
@@ -2,6 +2,10 @@
 #library(parallel)
 library(snow)
 library(pvclust)
+source(paste(Sys.getenv("PATH_TO_SGE_SCRIPTS"),"pvrect.bigRedButton.R",sep="/"))
+source(paste(Sys.getenv("PATH_TO_SGE_SCRIPTS"),"pvclust.bigRedButton.R",sep="/"))   
+source(paste(Sys.getenv("PATH_TO_SGE_SCRIPTS"),"parPvclust.bigRedButton.R",sep="/"))
+source(paste(Sys.getenv("PATH_TO_SGE_SCRIPTS"),"pvpick.bigRedButton.R",sep="/"))   
 ############
 args <- commandArgs(TRUE)
 # Use fread() instead, much faster. Make sure the data is clean! (data.table, plyr and dplyr libraries)
@@ -14,6 +18,8 @@ Ids<-read.table(args[2])[,1]
 ############
 colnames(Matrix.df)<-Ids
 #args[3] = # CPUs
+# convert similarity matrix to distance matrix
+distances <- as.dist(1 - Matrix.df)
 ############
 print("[ R ] ...creating parallel environemnt")
 cluster <- makeSOCKcluster(rep("localhost", args[3]))
@@ -22,7 +28,8 @@ clusterExport(cluster, ls())
 print("[ R ] ...performing clustering and bootstrapping ")
 # runs bootstrapping in parallel
 boots <- as.numeric(args[4])
-result <- parPvclust( cluster, Matrix.df, method.dist="cor", method.hclust="average", nboot=boots)
+result <- parPvclust.bigRedButton( cluster, Matrix.df, distances, method.dist="cor", method.hclust="average", nboot=boots) 
+#result <- parPvclust( cluster, Matrix.df, method.dist="cor", method.hclust="average", nboot=boots)
 stopCluster(cluster)
 # print out dendrograms
 print("[ R ] ...preparing ouput")
@@ -35,6 +42,7 @@ outfile <- gsub(".matrix","",args[1])
 # dev.off()
 ############
 alpha <- as.numeric(args[5])
+beta <- as.numeric(args[6])
 print("[ R ] ...creating pdf file")
 out <- paste0(outfile,"_a",alpha,".pdf")
 pdf( out ,width=ncol(Matrix.df)/3,height=10)
@@ -53,7 +61,7 @@ list2ascii <- function(x,file=paste(deparse(substitute(x)),".txt",sep="")) {
 	return(invisible(NULL))       # return (nothing) from function
 }
 #list2ascii(pvpick(result, alpha=0.95, pv="au", type="geq", max.only=TRUE)$clusters, file=paste0(outfile,"_a95_clusters"))
-list2ascii(pvpick(result, alpha=alpha, pv="au", type="geq", max.only=TRUE)$clusters, file=paste0(outfile,"_a",alpha,"_clusters"))
+list2ascii(pvpick.bigRedButton(result, alpha=alpha, beta=beta, type="geq", max.only=TRUE)$clusters, file=paste0(outfile,"_a",alpha,"_clusters"))   
 ############
 print("[ R ] ...creating newick trees from pvclust output")
 library(ape)
